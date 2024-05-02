5c029cf5f25ee6e2a5c55349cec74287aa91325a
s-woegerbauer <120839159+s-woegerbauer@users.noreply.github.com>
2024-05-02 11:59:10 +0200

    Added API requests

--- js/lobby.js
+++ js/lobby.js
@@ -1,24 +1,65 @@
+const defaultUrl = 'https://localhost:3000';
+
 function login()
 {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    // TODO: Add API request
+
+    fetch(defaultUrl + `/Player/Login?username=${username}&password=${password}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }})
+        .catch((error) => {
+            console.log(error)
+        });
 }
 
 function register()
 {
     const newUsername = document.getElementById('newUsername').value;
     const newPassword = document.getElementById('newPassword').value;
-    // TODO: Add API request
+
+    fetch(defaultUrl + `/Player/Login?username=${newUsername}&password=${newPassword}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }})
+        .catch((error) => {
+            console.log(error)
+        })
+        .then(() => {
+            document.getElementById('username').value = newUsername;
+            document.getElementById('password').value = newPassword;
+            document.getElementById('login').click();
+        });
 }
 
 function host()
 {
-    // TODO: Add API request
+    fetch(defaultUrl + `/Lobby/Host`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }})
+        .catch((error) => {
+            console.log(error)
+        });
 }
 
 function join()
 {
     const gameCode = document.getElementById('gameCode').value;
-    // TODO: Add API request
+
+    fetch(defaultUrl + `/Player/JoinLobby?`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            gameCode: gameCode
+        })})
+        .catch((error) => {
+            console.log(error)
+        });
 }
